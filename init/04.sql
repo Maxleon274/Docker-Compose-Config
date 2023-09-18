ac87f36bb7f57ed60e94c3c5bd605c5da46499da
Max <max.rosemeier@gmail.com>
2023-09-18 17:13:02 +0200

    fix: sql creates in seperate files

---
+++ init/04.sql
@@ -0,0 +1,16 @@
+CREATE TABLE hausrat
+(
+    trigger_id         BIGINT,
+    zahlungs_intervall int,
+    produkt_id         varchar(255),
+    sb                 int,
+    plz                int,
+    wohnflaeche        BIGINT,
+    baustein1          BIGINT,
+    baustein2          BIGINT,
+    baustein3          BIGINT,
+    baustein4          BIGINT,
+    baustein5          BIGINT,
+    baustein6          BIGINT,
+    baustein7          BIGINT
+);
